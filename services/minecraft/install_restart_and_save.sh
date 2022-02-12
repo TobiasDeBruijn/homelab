0fa14b9eb6f6b9e06e2126c798b47cbfb14cf0b7
Tobias de Bruijn <t.debruijn@array21.dev>
2022-02-12 11:33:26 +0100

    Update install_restart_and_save.sh

--- services/minecraft/install_restart_and_save.sh
+++ services/minecraft/install_restart_and_save.sh
@@ -14,10 +14,10 @@ wget -q https://raw.githubusercontent.com/TheDutchMC/homelab/main/services/minec
 chown -R mcserver:mcserver /data/scripts/
 chmod u+rwx -R /data/scripts/
 
-echo "$(echo '45 5,11,17,23 * * * /data/scripts/restart_15.sh > /data/scripts/cronlog/restart_warning_15.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
-echo "$(echo '55 5,11,17,23 * * * /data/scripts/restart_5.sh > /data/scripts/cronlog/restart_warning_5.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
-echo "$(echo '59 5,11,17,23 * * * /data/scripts/restart_1.sh > /data/scripts/cronlog/restart_warning_1.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
-echo "$(echo '0 0,6,12,18 * * * /data/scripts/restart.sh > /data/scripts/cronlog/restart.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
-echo "$(echo '*/20 * * * * /data/scripts/save.sh > /data/scripts/cronlog/save.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
+#echo "$(echo '45 5,11,17,23 * * * /data/scripts/restart_15.sh > /data/scripts/cronlog/restart_warning_15.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
+#echo "$(echo '55 5,11,17,23 * * * /data/scripts/restart_5.sh > /data/scripts/cronlog/restart_warning_5.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
+#echo "$(echo '59 5,11,17,23 * * * /data/scripts/restart_1.sh > /data/scripts/cronlog/restart_warning_1.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
+#echo "$(echo '0 0,6,12,18 * * * /data/scripts/restart.sh > /data/scripts/cronlog/restart.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
+#echo "$(echo '*/20 * * * * /data/scripts/save.sh > /data/scripts/cronlog/save.log' ; crontab -u mcserver -l)" | crontab -u mcserver -
 
 echo "Done."
